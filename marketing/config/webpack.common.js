9c088da86a3964e6261099562b85dd9fe224cc3e
otaram <ousmane.taram@africasys.com>
2021-03-03 20:53:59 +0100

    add workflow

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpakPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
       rules: [
@@ -14,4 +16,10 @@ module.exports = {
         },
       ],
     },
+    plugins: [
+      new HtmlWebpakPlugin({
+          template: './public/index.html'
+      }),
+
+  ]
   }
